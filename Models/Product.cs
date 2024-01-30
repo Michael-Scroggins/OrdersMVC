45c3e5403b4f7ce640ed2ee5f614d6cb57f568c2
Michael-Scroggins <j.mikescroggins@gmail.com>
2024-01-30 14:10:07 -0500

    Added data annotations to the Product model properties

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,13 +1,18 @@
-﻿namespace LogisticsMVC.Models
+﻿using System.ComponentModel;
+
+namespace LogisticsMVC.Models
 {
     public class Product
     {
+        [DisplayName("Product ID")]
         public int ProductId { get; set; }
+        [DisplayName("Product Name")]
         public string ProductName { get; set; }
+        [DisplayName("Product Description")]
         public string ProductDescription { get; set; }
-
+        [DisplayName("Product Quantity Stocked")]
         public int ProductQuantityInStock { get; set; }
-
+        [DisplayName("Product Price")]
         public decimal ProductPrice { get; set; }
 
     }
