adf08f34a340872c7aa9ddd340d9a28fffcf7b90
Michael-Scroggins <j.mikescroggins@gmail.com>
2024-01-30 15:38:36 -0500

    Added Order and OrderItem models

---
+++ Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LogisticsMVC.Models
+{
+
+    public enum OrderType
+    {
+        Pick,
+        Store
+    }
+
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public OrderType Type { get; set; }
+
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+    }
+
+}
---
+++ Models/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿namespace LogisticsMVC.Models
+{
+    public class OrderItem
+    {
+            public int OrderItemID { get; set; }
+
+            public int QuantityInOrder { get; set; }
+            public int ProductID { get; set; }
+            
+            public virtual Product Product { get; set; }
+           
+            public int OrderID { get; set; }
+
+           public virtual Order Order { get; set; } 
+
+    }
+}
