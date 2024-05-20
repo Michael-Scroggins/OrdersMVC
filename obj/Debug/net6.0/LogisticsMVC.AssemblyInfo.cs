411cf7ef45f93afc1b4e498f229400de7c260e49
Michael-Scroggins <j.mikescroggins@gmail.com>
2024-05-19 23:38:48 -0400

    Changed Inventory asp-action to 'Index'

--- obj/Debug/net6.0/LogisticsMVC.AssemblyInfo.cs
+++ obj/Debug/net6.0/LogisticsMVC.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogisticsMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8e93126034872fa6b0d0c655a464d1b24beeab")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogisticsMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogisticsMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
