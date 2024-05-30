4486c3eed1edead2eacd8e005ade6f0445a6caeb
Michael-Scroggins <j.mikescroggins@gmail.com>
2024-05-30 11:05:22 -0400

    Removed Data Annotations in OrderProduct model in preparation to use Fluent API to configure composite keys

--- Models/OrderProduct.cs
+++ Models/OrderProduct.cs
@@ -1,14 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-namespace LogisticsMVC.Models
+﻿namespace LogisticsMVC.Models
 {
     public class OrderProduct
     {
-        [Key, Column(Order = 0)]
         public int OrderId { get; set; }
         public Order Order { get; set; }
 
-        [Key, Column(Order = 1)]
         public int ProductId { get; set; }
         public Product Product { get; set; }
 
