d1705966cad2ce805ee0ddd40c535a2d5de655ec
Michael-Scroggins <j.mikescroggins@gmail.com>
2024-06-02 19:36:10 -0400

    SecondTestMigration inclusion

--- Data/Migrations/20240602232857_SecondTestMigration.Designer.cs
+++ Data/Migrations/20240602232857_SecondTestMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrdersMVC.Data;
 
@@ -11,9 +12,10 @@ using OrdersMVC.Data;
 namespace OrdersMVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240602232857_SecondTestMigration")]
+    partial class SecondTestMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
